9914b4b3bb26bfec441b56a24d2b1e42829e3aaa
Tony <a798312002@hotmail.com>
2016-05-14 17:21:33 +0800

    保哥第一堂

--- EFConsole/Program.cs
+++ EFConsole/Program.cs
@@ -8,14 +8,85 @@ namespace EFConsole
 {
     class Program
     {
+        public class DepartmentCourse
+        {
+            public int  DepartmentID { get; set; }
+            public string DepartmentName { get; set; }
+            public string Title { get; set; }
+        }
+
+
         static void Main(string[] args)
         {
             using (var db = new ContosoUniversityEntities())
             {
-               //EF基本操作練習(db);
+                //EF基本操作練習(db);
+
+                //EF基本操作練習2(db);
+            }
+        }
+
+        private static void EF基本操作練習2(ContosoUniversityEntities db)
+        {
+            #region 延遲載入
+
+            //關閉代理物件(POCO Dynamic Proxy)
+            db.Configuration.ProxyCreationEnabled = false;
+
+            //延遲載入
+            var one = db.Course.FirstOrDefault(m => m.DepartmentID == 1);
+
+            Console.WriteLine(one.Title + "\t" + one.Department.Name);
+
+            //非延遲載入
+            foreach (var person in db.Person.Include("Department"))
+            {
+                Console.WriteLine(person.FirstName + person.LastName);
+
+                foreach (var course in person.Course)
+                {
+                    Console.WriteLine("\t" + course.Title);
+                }
             }
 
-            Console.ReadLine();
+            #endregion
+
+            #region  Native SQL
+
+            var sql = "select a.DepartmentID, a.Name as DepartmentName, b.Title from Department as A left join Course as B on A.DepartmentID = B.DepartmentID";
+
+            var data = db.Database.SqlQuery<DepartmentCourse>(sql);
+
+            foreach (var item in data)
+            {
+                Console.WriteLine(item.DepartmentName + "\t" + item.Title);
+            }
+
+            #endregion
+
+            #region Entity Framework with View
+
+            var view1 = db.vwDepartmentCourse;
+
+            foreach (var item in view1)
+            {
+                Console.WriteLine(item.DepartmentName + "\t" + item.Title);
+            }
+
+            #endregion
+
+            #region EF default Value(with T4)
+
+            var newData = new Course()
+            {
+                Title = "Test123",
+                DepartmentID = 5
+            };
+
+            db.Course.Add(newData);
+            db.SaveChanges();
+
+            #endregion
         }
 
         private static void EF基本操作練習(ContosoUniversityEntities db)
